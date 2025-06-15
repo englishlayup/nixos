bae55e0b00e82b3c901f8e06bf75dbc4e4a3075f
Duc Tran <duc.tran2027@gmail.com>
2025-06-15 10:10:06 -0400

    28 current  2025-06-15 10:10:04  25.11.20250603.c2a0396     6.12.31                          *

--- host/framework-13/configuration.nix
+++ host/framework-13/configuration.nix
@@ -180,7 +180,7 @@
 
   programs.fish.enable = true;
 
-  programs.hyprland.enable = true;
+  programs.hyprland.withUWSM  = true;
 
   # Hint Electron apps to use Wayland:
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
