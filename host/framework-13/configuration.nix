fe6aa0b6db6b76519837df57264c5482a3533c04
Duc Tran <duc.tran2027@gmail.com>
2025-07-17 23:48:19 -0400

    73 current  2025-07-17 23:48:15  25.11.20250603.c2a0396  6.12.31                          *

--- host/framework-13/configuration.nix
+++ host/framework-13/configuration.nix
@@ -70,41 +70,41 @@
   # Console keymap (for TTY)
   console.useXkbConfig = true; # Use X11 keymap settings for console
 
-  # services.kanata = {
-  #   enable = true;
-  #   keyboards = {
-  #     internalKeyboard = {
-  #       devices = [
-  #         "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
-  #       ];
-  #       extraDefCfg = "process-unmapped-keys yes";
-  #       config = ''
-  #         (defsrc
-  #          caps a s d f j k l ;
-  #         )
-  #         (defvar
-  #          tap-time 250
-  #          hold-time 300
-  #         )
-  #         (defalias
-  #          caps (tap-hold 100 200 esc lctl)
-  #          a (tap-hold $tap-time $hold-time a lmet)
-  #          s (tap-hold $tap-time $hold-time s lalt)
-  #          d (tap-hold $tap-time $hold-time d lsft)
-  #          f (tap-hold $tap-time $hold-time f lctl)
-  #          j (tap-hold $tap-time $hold-time j rctl)
-  #          k (tap-hold $tap-time $hold-time k rsft)
-  #          l (tap-hold $tap-time $hold-time l ralt)
-  #          ; (tap-hold $tap-time $hold-time ; rmet)
-  #         )
-  #
-  #         (deflayer base
-  #          @caps @a  @s  @d  @f  @j  @k  @l  @;
-  #         )
-  #       '';
-  #     };
-  #   };
-  # };
+  services.kanata = {
+    enable = true;
+    keyboards = {
+      internalKeyboard = {
+        devices = [
+          "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
+        ];
+        extraDefCfg = "process-unmapped-keys yes";
+        config = ''
+          (defsrc
+           caps a s d f j k l ;
+          )
+          (defvar
+           tap-time 250
+           hold-time 300
+          )
+          (defalias
+           caps (tap-hold 100 200 esc lctl)
+           a (tap-hold $tap-time $hold-time a lmet)
+           s (tap-hold $tap-time $hold-time s lalt)
+           d (tap-hold $tap-time $hold-time d lsft)
+           f (tap-hold $tap-time $hold-time f lctl)
+           j (tap-hold $tap-time $hold-time j rctl)
+           k (tap-hold $tap-time $hold-time k rsft)
+           l (tap-hold $tap-time $hold-time l ralt)
+           ; (tap-hold $tap-time $hold-time ; rmet)
+          )
+
+          (deflayer base
+           @caps @a  @s  @d  @f  @j  @k  @l  @;
+          )
+        '';
+      };
+    };
+  };
 
   # Enable screensharing with pipewire
   security.rtkit.enable = true;
