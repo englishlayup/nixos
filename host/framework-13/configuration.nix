9dfb9e99f57684748f647ec96eff523b6dba2163
Duc Tran <duc.tran2027@gmail.com>
2025-08-03 10:10:54 -0400

    93 current  2025-08-03 10:10:48  25.11.20250603.c2a0396  6.12.31                          *

--- host/framework-13/configuration.nix
+++ host/framework-13/configuration.nix
@@ -153,31 +153,18 @@
   };
 
   services.kanata = {
-    enable = false;
+    enable = true;
     keyboards = {
       internalKeyboard = {
         config = ''
           (defsrc
-           caps a s d f j k l ;
-          )
-          (defvar
-           tap-time 200
-           hold-time 200
+           caps
           )
           (defalias
            caps (tap-hold 100 200 esc lctl)
-           a (tap-hold $tap-time $hold-time a lmet)
-           s (tap-hold $tap-time $hold-time s lalt)
-           d (tap-hold $tap-time $hold-time d lsft)
-           f (tap-hold $tap-time $hold-time f lctl)
-           j (tap-hold $tap-time $hold-time j rctl)
-           k (tap-hold $tap-time $hold-time k rsft)
-           l (tap-hold $tap-time $hold-time l ralt)
-           ; (tap-hold $tap-time $hold-time ; rmet)
           )
-
           (deflayer base
-           @caps @a  @s  @d  @f  @j  @k  @l  @;
+           @caps
           )
         '';
       };
